0ed8e1f928f4691208424639b0de565aaef08944
AdorkablyNicole <hello@nicolegeorge.me>
2021-02-27 18:21:46 +1030

    capturing new list items and creating a new li element - working in console

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ form.addEventListener('submit', addItem);
 
 // Add item
 function addItem(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-  console.log(1);
+  //  get input value
+  const newItem = document.getElementById('item');
+
+  //   Create new <li> element to capture a new list input
+  const newListItem = document.createElement('li');
+  // add class to the new element
+  newListItem.className = 'list-group-items';
+
+  console.log(newListItem);
 }
