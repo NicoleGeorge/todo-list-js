9a86c54a6784b476fd151da73f1b53e34fc1fa8a
AdorkablyNicole <hello@nicolegeorge.me>
2021-02-27 18:31:12 +1030

    delete button now appearing within the li

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addItem(e) {
   e.preventDefault();
 
   //  get input value
-  const newItem = document.getElementById('item');
+  const newItem = document.getElementById('item').value;
 
   //   Create new <li> element to capture a new list input
   const newListItem = document.createElement('li');
@@ -19,8 +19,16 @@ function addItem(e) {
   // console.log(newListItem);
 
   //   Add new list item value to the list
-  newListItem.appendChild(document.createTextNode(newItem.value));
+  newListItem.appendChild(document.createTextNode(newItem));
 
+  //   create the delete button element
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-small float-right delete';
+
+  //   Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  //   Add the button to the li element
+  newListItem.appendChild(deleteBtn);
   itemList.appendChild(newListItem);
-  
 }
